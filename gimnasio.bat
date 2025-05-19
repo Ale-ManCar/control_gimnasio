62a5bbd4b4e24cd85e9430b3e2558c144ceef16c
Mancar <alemancar0511@gmail.com>
2025-05-19 13:49:34 -0500

    Added Service Installer

---
+++ gimnasio.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "C:\Users\alema\IdeaProjects\projects\gimnasio_control"
+java --module-path "C:\java\javafx-sdk-21\lib" --add-modules javafx.controls,javafx.fxml -jar gimnasio-control.jar
